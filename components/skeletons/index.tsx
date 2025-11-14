0b588e387a6f4b068336dfd0088645a4ad8ad61c
Alexandre GOUMAIN <alexandre26goumain@gmail.com>
2025-11-15 00:20:12 +0100

    chore: remove obsolete skeleton files
    - Delete 7 duplicate skeleton components (client portal and dashboard)
    - Update skeletons index to remove obsolete exports
    - All functionality now handled by centralized PageSkeleton and GridSkeleton
    - Reduces code duplication and maintenance burden

--- components/skeletons/index.tsx
+++ components/skeletons/index.tsx
@@ -1,6 +1,3 @@
 export { ArticleCardSkeleton } from "./article-card-skeleton";
 export { CardSkeleton } from "./card-skeleton";
-export { ClientsPageSkeleton } from "./clients-page-skeleton";
-export { DashboardSkeleton } from "./dashboard-skeleton";
-export { SettingsPageSkeleton } from "./settings-page-skeleton";
 export { TableSkeleton } from "./table-skeleton";
