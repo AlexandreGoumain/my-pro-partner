1afec898ff0f117505e3255f3da636f5c16f7d01
Alexandre GOUMAIN <alexandre26goumain@gmail.com>
2025-11-14 10:47:19 +0100

    Remove deprecated loading components
    - Remove LoadingCard and LoadingFallback UI components
    - Remove specialized loading-state components
    - Replaced by new unified LoadingState component

--- components/bank-reconciliation/loading-state.tsx
+++
@@ -1,7 +0,0 @@
-export function LoadingState() {
-    return (
-        <div className="text-center py-12">
-            <p className="text-[14px] text-black/40">Chargement...</p>
-        </div>
-    );
-}
