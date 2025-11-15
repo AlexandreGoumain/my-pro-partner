07c315cd8b32373600f4f7a28022f21e8c1b45ed
Alexandre GOUMAIN <alexandre26goumain@gmail.com>
2025-11-15 01:03:29 +0100

    Clean up invalid export in personnel module index
    - Remove export for deleted personnel-search-bar component
    - Fix module exports to match existing components

--- components/personnel/index.tsx
+++ components/personnel/index.tsx
@@ -2,10 +2,9 @@
  * Export centralisé des composants du module personnel
  */
 
-export * from "./personnel-stats-grid";
-export * from "./personnel-search-bar";
-export * from "./personnel-list-item";
 export * from "./personnel-dialogs";
+export * from "./personnel-list-item";
+export * from "./personnel-stats-grid";
 export * from "./role-badge";
 export * from "./status-badge";
 export * from "./user-form";
